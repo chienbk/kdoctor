755ec4a2f1d47dd7df46d8718492c7d1e44ca44b
CHIENBK\ChienBK <nvc.hut@gmail.com>
2018-04-18 07:30:17 +0700

    Update code

---
+++ app/src/main/java/thebrightcompany/com/kdoctor/App.java
@@ -0,0 +1,6 @@
+package thebrightcompany.com.kdoctor;
+
+import android.app.Application;
+
+public class App extends Application{
+}
---
+++ app/src/main/java/thebrightcompany/com/kdoctor/view/SplashActivity.java
@@ -0,0 +1,15 @@
+package thebrightcompany.com.kdoctor.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import thebrightcompany.com.kdoctor.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
